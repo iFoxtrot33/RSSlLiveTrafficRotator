786bf6f9facc6411f98c9c4ea29ce740c2001e2f
iurii <furmaniurii@gmail.com>
2022-12-17 12:44:51 +0400

    Finish updates logic

--- src/main.js
+++ src/main.js
@@ -12,22 +12,46 @@ const loadXMLFeed = () => {
 document.addEventListener('DOMContentLoaded', loadXMLFeed());
 
 function displayTrafficList(x) {
-  let list = document.getElementById('item');
-  let item = x.getElementsByTagName('item');
-  let itemNum = x.getElementsByTagName('item').length;
+  const list = document.getElementById('item');
+  const item = x.getElementsByTagName('item');
+  const itemNum = x.getElementsByTagName('item').length;
 
-  
+  if (itemNum === 0) {
+    list.innerHTML = '<li><h3>No Planned Roadworks</h3><li>';
 
-  for (let i = 0; i < itemNum; i += 1) {
-    let li = document.createElement('li');
-    li.className = 'listItem';
+    setTimeout(() => {
+      loadXMLFeed();
+      console.log('No new data...');
+    }, 7000);
+  } else {
+    for (let i = 0; i < itemNum; i += 1) {
+      const li = document.createElement('li');
+      li.className = 'listItem';
 
-    li.innerHTML = `
-    <h3>${item[i].getElementsByTagName('title')[0].innerHTML}</h3>
-    <p>${item[i].getElementsByTagName('description')[0].innerHTML}</p>
-    `;
+      li.innerHTML = `
+        <h3>${item[i].getElementsByTagName('title')[0].innerHTML}</h3>
+        <p>${item[i].getElementsByTagName('description')[0].innerHTML}</p>
+        `;
 
-    list.appendChild(li);
+      list.appendChild(li);
+    }
   }
-
+  setTimeout(() => {
+    startAnimation(item);
+  }, 7000);
 }
+
+const startAnimation = (x) => {
+  const listItem = document.getElementsByClassName('listItem');
+
+  for (let i = 0; i < x.length; i += 1) {
+    setTimeout(() => {
+      listItem[0].remove();
+
+      if (i === x.length - 1) {
+        loadXMLFeed();
+        console.log('Loading New Data...');
+      }
+    }, i * 3000);
+  }
+};
